8550affee77d7620ae6abfbdc19f85caa334f409
sidv1905 <sidvarangaonkar1905@gmail.com>
2024-06-02 17:48:38 +0530

    feat: scrollable section pills + blog 90% completion

--- src/components/SectionPills.tsx
+++ src/components/SectionPills.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import styles from './styles/sectionPills.module.css';
 
 interface SectionPillsProps {
@@ -5,18 +6,36 @@ interface SectionPillsProps {
 }
 
 export default function SectionPills(props: SectionPillsProps) {
-  const allSections = props.allPosts.map((post) => {
+  const containerRef = useRef(null);
+
+  const scrollContainerRight = () => {
+    containerRef.current.scrollBy(100, 0); // Scroll right by 50 pixels
+  };
+
+  const scrollContainerLeft = () => {
+    containerRef.current.scrollBy(-100, 0); // Scroll left by 50 pixels
+  };
+  let allSections = props.allPosts.map((post) => {
     return post.frontmatter.topic;
   });
+
   return (
     <div className={styles.pillsContainer}>
-      {allSections.map((section) => {
-        return (
-          <a href={`/blog/${section}`} className={`${styles.pillItem}`}>
-            {section}
-          </a>
-        );
-      })}
+      <div className={styles.lefter} onClick={scrollContainerLeft}>
+        <p>&lt;</p>
+      </div>
+      <div className={styles.pills} ref={containerRef}>
+        {allSections.map((section) => {
+          return (
+            <a href={`/blog/${section}`} className={`${styles.pillItem}`}>
+              {section}
+            </a>
+          );
+        })}
+      </div>
+      <div className={styles.righter} onClick={scrollContainerRight}>
+        <p>&gt;</p>
+      </div>
     </div>
   );
 }
