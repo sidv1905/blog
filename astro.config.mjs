324120ada9686465abb21229380af850fe07f911
Siddharth <sidvarangaonkar1905@gmail.com>
2025-03-10 12:00:35 +0530

    deploy v0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import mdx from "@astrojs/mdx";
-import remarkGfm from "remark-gfm";
-import remarkSmartypants from "remark-smartypants";
-import rehypeExternalLinks from "rehype-external-links";
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
+import remarkSmartypants from 'remark-smartypants';
+import rehypeExternalLinks from 'rehype-external-links';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-template.netlify.app",
+  site: 'https://sidblogs.github.io',
+  base: 'blog',
   integrations: [mdx(), svelte(), react()],
   markdown: {
     shikiConfig: {
-      theme: "nord"
+      theme: 'nord',
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: "_blank"
-    }]]
-  }
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
 });
