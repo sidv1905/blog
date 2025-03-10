5b2154a26e7bd315d0be659f85251c6666cdc4a8
Siddharth <sidvarangaonkar1905@gmail.com>
2025-03-10 12:28:52 +0530

    feat: fixes and cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sidblogs.github.io',
-  base: 'blog',
+  base: '',
   integrations: [mdx(), svelte(), react()],
   markdown: {
     shikiConfig: {
--- src/components/SectionPills.tsx
+++ src/components/SectionPills.tsx
@@ -9,11 +9,11 @@ export default function SectionPills(props: SectionPillsProps) {
   const containerRef = useRef(null);
 
   const scrollContainerRight = () => {
-    containerRef.current.scrollBy(100, 0); // Scroll right by 50 pixels
+    containerRef?.current.scrollBy(100, 0); // Scroll right by 50 pixels
   };
 
   const scrollContainerLeft = () => {
-    containerRef.current.scrollBy(-100, 0); // Scroll left by 50 pixels
+    containerRef?.current.scrollBy(-100, 0); // Scroll left by 50 pixels
   };
   let allSections = props.allPosts.map((post) => {
     return post.frontmatter.topic;
