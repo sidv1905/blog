e602fd739c4d35aba992a8347cabf10747420a90
sidv1905 <sidvarangaonkar1905@gmail.com>
2025-05-29 06:36:55 +0000

    feat: analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,25 @@ import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
 import remarkSmartypants from 'remark-smartypants';
 import rehypeExternalLinks from 'rehype-external-links';
-
+import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sidv1905.github.io',
   base: '/blog',
-  integrations: [mdx(), svelte(), react()],
+  integrations: [
+    mdx(),
+    svelte(),
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'nord',
